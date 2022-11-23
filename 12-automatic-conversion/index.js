6b5d7e1f93fb1da8930999e65cbfb37c8602e5d8
Lucassec1 <lucasemmanuelcs@gmail.com>
2022-11-23 18:31:19 -0300

    :sparkles: 12-automatic-conversion

---
+++ 12-automatic-conversion/index.js
@@ -0,0 +1,9 @@
+console.log("123" + 4);
+console.log("22" - 2);
+console.log("test" * 7);
+console.log(9 * null);
+
+console.log(typeof ("123" + 4));
+console.log(typeof ("22" - 2));
+console.log(typeof ("test" * 7));
+console.log(typeof (9 * null));
