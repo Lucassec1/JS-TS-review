7a43ad85db42f9d7fb540a4766e7822c8e837b0d
Lucassec1 <lucasemmanuelcs@gmail.com>
2022-11-23 17:18:39 -0300

    :sparkles: 7-type-boolean

---
+++ 7-type-boolean/index.js
@@ -0,0 +1,9 @@
+console.log(true);
+console.log(false);
+console.log(16 > 4);
+console.log(7 < 2);
+
+console.log(typeof true);
+console.log(typeof false);
+console.log(typeof (16 > 4));
+console.log(typeof (7 < 2));
