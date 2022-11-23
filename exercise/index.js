15ad4f9ee51bc94100e7470b462550ba08c76cc5
Lucassec1 <lucasemmanuelcs@gmail.com>
2022-11-23 19:51:44 -0300

    :sparkles: solving questions from module 1

---
+++ exercise/index.js
@@ -0,0 +1,27 @@
+// Write three values ​​in string
+
+console.log("double quote");
+console.log('single quote');
+console.log(`template literals`);
+
+// Write three values in number
+
+console.log(21);
+console.log(11.9);
+console.log(21 + 11.9);
+
+// Write three comparisons with boolean
+
+console.log(157 > 126);
+console.log(100 <= 181);
+console.log(95 != 96);
+
+// write three comparisons with logical operators
+
+console.log("Paula" == "Paula" && "8" === 8);
+console.log(12 >= 8 || 73 != 73);
+console.log(!(89 <= 33));
+
+// Do an operation that mimics a NaN
+
+console.log("Erick" * 20);
