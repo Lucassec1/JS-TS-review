6ebf9fb3910cdc0eeb9fd4a9027caf04974cf14b
Lucassec1 <lucasemmanuelcs@gmail.com>
2022-11-23 17:55:58 -0300

    9-logic-operators

---
+++ 9-logic-operators/index.js
@@ -0,0 +1,8 @@
+console.log(5 > 3 && 3 > 2);
+console.log(1 >= 2 && 7 === "7");
+console.log(5 == 5 && 2 >= 7);
+console.log(5 >= 2 || 9 <= 9);
+console.log(5 < 1 || 3 != 3);
+console.log(11 > 5 && 3 == 3);
+console.log(!(false && false));
+console.log(5 === 5 && 2 >= 7);
