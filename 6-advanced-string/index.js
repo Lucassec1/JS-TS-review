c8139d76f0c11b9a1bb46bc650d31088fa2be190
Lucassec1 <lucasemmanuelcs@gmail.com>
2022-11-23 17:09:43 -0300

    :sparkles: 6-advanced-string

---
+++ 6-advanced-string/index.js
@@ -0,0 +1,6 @@
+console.log("First line \n Second line");
+console.log("Her name is 'Paula'");
+console.log('His name is "Erick"');
+console.log(`The multiplication of 3 by 7 is ${3*7}`);
+console.log("My" + " name" + " is" + " Lucas");
+console.log("The multiplication of 3 by 7 is" + ` ${3*7}`);
