ac4e69bddbc020a6cf012a82990e2891114b42f9
Lucassec1 <lucasemmanuelcs@gmail.com>
2022-11-23 16:37:41 -0300

    :sparkles: 3-arithmetic-operations

---
+++ 3-arithmetic-operations/index.js
@@ -0,0 +1,13 @@
+console.log(10 + 16);
+console.log(21 - 8);
+console.log(11 * 4);
+console.log(115 / 5);
+console.log(45 % 9);
+console.log(20 % 7);
+
+console.log(typeof (10 + 16));
+console.log(typeof (21 - 8));
+console.log(typeof (11 * 4));
+console.log(typeof (115 / 5));
+console.log(typeof (45 % 9));
+console.log(typeof (20 % 7));
