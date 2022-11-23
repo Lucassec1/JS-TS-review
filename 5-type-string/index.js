cab2dc1a093ab0b1d5f8974a3a3db69e40093a57
Lucassec1 <lucasemmanuelcs@gmail.com>
2022-11-23 16:53:32 -0300

    :sparkles: 5-type-string

---
+++ 5-type-string/index.js
@@ -0,0 +1,7 @@
+console.log("Gabriel Toledo");
+console.log('Paula Naguil');
+console.log(`Erick Santos`);
+
+console.log(typeof "Gabriel Toledo");
+console.log(typeof 'Paula Naguil');
+console.log(typeof `Erick Santos`);
