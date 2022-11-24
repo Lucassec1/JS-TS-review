28db3848ebbf0eca777086a5852c6f3e9b204686
Lucassec1 <lucasemmanuelcs@gmail.com>
2022-11-24 09:32:41 -0300

    :sparkles: 9-conditional-if

---
+++ second module/9-conditional-if/index.js
@@ -0,0 +1,14 @@
+let age = 19;
+let peopleName = "Paula";
+
+if(age == 19) {
+    console.log("You are young");
+}
+
+if(age < 19) {
+    console.log("You are possibly a child")
+}
+
+if(peopleName == "Paula" && age > 10) {
+    console.log("That is right")
+}
