a16d4932cf83ba526eadbe3dcc746c34d9486a67
Lucassec1 <lucasemmanuelcs@gmail.com>
2022-11-24 09:42:50 -0300

    11-conditional-else-if

---
+++ second module/11-conditional-else-if/index.js
@@ -0,0 +1,12 @@
+let a = 6;
+let b = 4;
+
+if (a * b == 24) {
+    console.log("The result is 24")
+} else if (b == 5) {
+    console.log("The result now is 30")
+} else if (a == 5) {
+    console.log("The result now is 20")
+} else {
+    console.log("Do not have results")
+}
