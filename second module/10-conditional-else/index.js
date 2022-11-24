f0b41ca7de9319904fddd0fd6b6e818f7941d598
Lucassec1 <lucasemmanuelcs@gmail.com>
2022-11-24 09:38:38 -0300

    10-conditional-else

---
+++ second module/10-conditional-else/index.js
@@ -0,0 +1,15 @@
+let age = 19;
+
+if (age == 19) {
+    console.log("You are young")
+} else {
+    console.log("You is possibly a child elderly")
+}
+
+let velocity = 90;
+
+if (velocity >= 60) {
+    console.log("Fined")
+} else {
+    console.log("Not fined")
+}
