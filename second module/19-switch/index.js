07233b0bdf9ee0ad013b9398caff9a7068cfb03b
Lucassec1 <lucasemmanuelcs@gmail.com>
2022-11-24 11:18:49 -0300

    :sparkles: 19-switch

---
+++ second module/19-switch/index.js
@@ -0,0 +1,19 @@
+
+let cars = ['Tesla', 'BMW', 'Ferrari', 'Toyota'];
+
+cars = "BMW";
+
+switch (cars) {
+    case "Tesla":  
+        console.log('The car is Tesla');
+        break;
+    case "BMW":
+        console.log('The car is BMW');
+        break;
+    case "Ferrari":
+        console.log('The car is Ferrari');
+        break;
+    default: 
+        console.log('The car is Toyota');
+        break;
+}
