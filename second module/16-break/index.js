d40a3a9135ac8d8f4b3bc2b05e8a36d49a60513c
Lucassec1 <lucasemmanuelcs@gmail.com>
2022-11-24 10:30:58 -0300

    16-break

---
+++ second module/16-break/index.js
@@ -0,0 +1,10 @@
+for (let x = 1; x <= 100; i++) {
+    if (x == 10) {
+        console.log("arrived at 10")
+    } 
+    
+    if (x == 30) {
+        console.log("arrived at 30")
+        break;
+    }
+}
