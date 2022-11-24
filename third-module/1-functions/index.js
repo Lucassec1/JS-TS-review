22808a8e4ca3e65525118c67f443d9a47a39672c
Lucassec1 <lucasemmanuelcs@gmail.com>
2022-11-24 15:45:52 -0300

    :spakles: 1-starting with functions

---
+++ third-module/1-functions/index.js
@@ -0,0 +1,21 @@
+function printOnConsole() {
+    console.log("Hello world!")
+}
+
+printOnConsole();
+
+function printANumber(num) {
+    console.log(`O número é: ${num}`)
+}
+
+printANumber(11);
+printANumber(4);
+printANumber(25);
+printANumber(-2);
+
+const randomNumber = function() {
+    console.log(Math.random());
+}
+
+randomNumber();
+randomNumber();
