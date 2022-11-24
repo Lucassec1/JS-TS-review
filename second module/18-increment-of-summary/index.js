f9e63a95a5afec6015e813febaa6a5c4eccaf562
Lucassec1 <lucasemmanuelcs@gmail.com>
2022-11-24 10:56:29 -0300

    :sparkles: 18-increment-of-summary

---
+++ second module/18-increment-of-summary/index.js
@@ -0,0 +1,15 @@
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+for (let x = 0; x < 20; x += 3) {
+    console.log(x);
+}
+
+for (let y = 10; y >= 0; y -= 2) {
+    console.log(`The value of y is ${y}`);
+}
+
+for (let z = 10; z > 0; z--) {
+    console.log(`The value of z is ${z}`);
+}
