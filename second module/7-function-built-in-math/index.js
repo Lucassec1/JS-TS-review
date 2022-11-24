ae07cb3731f534a0ec37d66e0c2219db33c7b868
Lucassec1 <lucasemmanuelcs@gmail.com>
2022-11-24 09:18:11 -0300

    :sparkles: 7-function-built-in-math

---
+++ second module/7-function-built-in-math/index.js
@@ -0,0 +1,11 @@
+let maxNumber = Math.max(5, 7, 11, 13);
+console.log(maxNumber);
+
+let minNumber = Math.min(5, 7, 11, 13);
+console.log(minNumber);
+
+let round = Math.round(3.141592653);
+console.log(round);
+
+let roundUp = Math.ceil(3.141592653);
+console.log(roundUp);
