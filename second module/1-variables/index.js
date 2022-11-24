2852f3a2fbea212a197ca32d0fa538f7307bac44
Lucassec1 <lucasemmanuelcs@gmail.com>
2022-11-24 08:36:05 -0300

    :sparkles: 1-variables

---
+++ second module/1-variables/index.js
@@ -0,0 +1,21 @@
+let people = "Erick";
+
+console.log(people);
+console.log(`His name is ${people}`);
+
+let strawberry = 7;
+
+console.log(strawberry * strawberry);
+console.log(strawberry + 1);
+
+strawberry = 9;
+console.log(strawberry);
+
+people = "Paula";
+console.log(people)
+
+strawberry = "strawberry";
+console.log(strawberry);
+
+let three = 3, four = 4, five = 5, six = 6;
+console.log((three + four) * (five + six));
