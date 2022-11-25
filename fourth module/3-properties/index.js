5389ac446622ae5eef0fa7a6b6a587cd9f034179
Lucassec1 <lucasemmanuelcs@gmail.com>
2022-11-25 08:40:36 -0300

    3-array-properties

---
+++ fourth module/3-properties/index.js
@@ -0,0 +1,7 @@
+let peopleName = "Erick";
+let numbers = [2, 4, 7, 11, 13, 17];
+
+console.log(peopleName.length);
+console.log(numbers.length);
+console.log(numbers[2]);
+console.log(numbers['length']);
