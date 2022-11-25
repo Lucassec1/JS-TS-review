a11bd73ba0428dd372bc80d56c1f6c928c9fd9d4
Lucassec1 <lucasemmanuelcs@gmail.com>
2022-11-25 10:12:53 -0300

    14-slice

---
+++ fourth module/14-slice/index.js
@@ -0,0 +1,16 @@
+let nums = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+console.log(nums.slice(4, 5));
+console.log(nums.slice(6, 8));
+console.log(nums.slice(5));
+
+let names = ['Joana', 'Cleber', 'Almir'];
+
+console.log(names.slice(1, 2));
+console.log(names.slice(1));
+
+names.push('Erick', 'Paula');
+console.log(names);
+
+console.log(names.slice(1, 2));
+console.log(names.slice(3, 4));
