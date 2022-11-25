bd393cd218e45fdc9d4295432482795771e5553c
Lucassec1 <lucasemmanuelcs@gmail.com>
2022-11-25 08:30:28 -0300

    1-array

---
+++ fourth module/1-array/index.js
@@ -0,0 +1,10 @@
+let array = [2, 4, 6, 8, 10];
+let names = ['Erick', 'Paula'];
+let booleans = [true, false];
+
+let boths = [true, 'Ana', 11];
+
+console.log(array);
+console.log(names);
+console.log(booleans);
+console.log(boths);
