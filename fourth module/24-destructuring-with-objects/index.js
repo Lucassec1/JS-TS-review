ddcb36a35863cb3f936d08520a366b6b513e7892
Lucassec1 <lucasemmanuelcs@gmail.com>
2022-11-25 11:15:30 -0300

    24-destructuring-with-objects

---
+++ fourth module/24-destructuring-with-objects/index.js
@@ -0,0 +1,10 @@
+let obj = {
+    wheels: 4,
+    doors: 4,
+    sunroof: true
+}
+
+const {wheels: vWheels, doors: vDoors, sunroof: vSunroof} = obj;
+
+console.log(vWheels);
+console.log(vSunroof);
