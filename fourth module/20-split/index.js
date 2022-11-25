be57b8cc1a8ddbb51668c11f25c80f94d08ee687
Lucassec1 <lucasemmanuelcs@gmail.com>
2022-11-25 10:41:17 -0300

    20-split

---
+++ fourth module/20-split/index.js
@@ -0,0 +1,8 @@
+let phrase = "testing the split method";
+console.log(phrase.split(" "));
+
+let fruits = 'banana;grape;strawberry;melon;orange';
+console.log(fruits.split(";"));
+
+let champion = "lee sin";
+console.log(champion.split(' '));
