3381d71a55268ae989f81b965aa50a58227e9b82
Lucassec1 <lucasemmanuelcs@gmail.com>
2022-11-25 08:32:40 -0300

    2-more-about-arrays

---
+++ fourth module/2-more-about-array/index.js
@@ -0,0 +1,10 @@
+let array = [2, 4, 6, 8, 10];
+let names = ['Erick', 'Paula'];
+let booleans = [true, false];
+
+let boths = [true, 'Ana', 11];
+
+console.log(array[1]);
+console.log(names[0]);
+console.log(booleans[0]);
+console.log(boths[2]);
