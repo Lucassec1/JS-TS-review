2d0d9a93131e37c36a60f199691a7615503b87a3
Lucassec1 <lucasemmanuelcs@gmail.com>
2022-11-25 08:45:21 -0300

    4-methods

---
+++ fourth module/4-methods/index.js
@@ -0,0 +1,11 @@
+let car = "bugatti";
+console.log(car.toUpperCase());
+
+let car2 = car.toUpperCase();
+console.log(car2.toLowerCase());
+
+// string
+console.log(typeof car2.toLowerCase());
+
+// function
+console.log(typeof car2.toLowerCase);
