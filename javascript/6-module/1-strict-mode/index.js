fccfb0e186db56393993ae6a20554a8d12aa48ec
Lucassec1 <lucasemmanuelcs@gmail.com>
2022-11-29 19:30:24 -0300

    :sparkles: feat: starting with methods of debug

---
+++ javascript/6-module/1-strict-mode/index.js
@@ -0,0 +1,17 @@
+"use strict";
+
+// fruit = 'orange';
+
+let fruit = 'strawberry';
+
+// delete object.prototype; 
+
+function test() {
+    "use strict"
+    let testing = 'test';
+}
+
+test();
+
+// false.prop = "";
+// 'teste'.prop = "";
---
+++ javascript/6-module/2-console-log/index.js
@@ -0,0 +1,20 @@
+let a = 1;
+let b = 2;
+let c = 3;
+
+if (b > c) {
+    a = b - 3
+    console.log(a);
+}
+
+
+for(let i = 0; i < a; i++) {
+    b++
+    b = c + a - 1;
+    console.log(b);
+}
+
+if (b = c + 3 ) {
+    console.log(a);
+    return a;
+}
