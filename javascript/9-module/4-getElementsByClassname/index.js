46d339d0c45762a7ae0e0d1053329609fbc11bc3
Lucassec1 <lucasemmanuelcs@gmail.com>
2022-11-30 11:31:59 -0300

    :sparkles: feat: class 1/5-dom-elements

---
+++ javascript/9-module/4-getElementsByClassname/index.js
@@ -0,0 +1,7 @@
+console.log(document.getElementsByTagName('li'));
+
+console.log(document.getElementsByClassName('blueItems'));
+
+console.log(document.getElementsByClassName('redItems'));
+
+console.log(document.getElementsByClassName('items'));
---
+++ javascript/9-module/5-querySelector/index.js
@@ -0,0 +1,11 @@
+console.log(document.querySelector('#main-container h1'));
+
+console.log(document.querySelector('h1'));
+
+console.log(document.querySelector('#second-container'));
+
+console.log(document.querySelector('div div p'));
+
+console.log(document.querySelector('footer h2'));
+
+console.log(document.querySelector('ul .blueItems'));
