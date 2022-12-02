260df598b41780a45135d52009e0a1bf05e83883
Lucassec1 <lucasemmanuelcs@gmail.com>
2022-12-02 11:12:07 -0300

    :sparkles: express get

--- javascript/12-module/4-express/index.js
+++ javascript/12-module/4-express/index.js
@@ -5,6 +5,10 @@ app.get('/', function(req, res) {
     res.send('first route with express');
 });
 
+app.get('/test', function(req, res) {
+    res.send('testing route');
+});
+
 app.listen(3000, function() {
     console.log('the application is running on the port 3000');
 });
