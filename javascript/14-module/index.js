d1b4c5d688d0b0042e52ed3e64cec4423f6dd3f0
Lucassec1 <lucasemmanuelcs@gmail.com>
2022-12-05 20:21:09 -0300

    :sparkles: feat: starting logic quizz-2

--- javascript/14-module/index.js
+++ javascript/14-module/index.js
@@ -9,68 +9,93 @@ let actualQuestion = 0;
 
 // Questions
 const questions = [
-    {
-        "question": "PHP was developed for what purpose?",
-        "answers": [
-            {
-                "answer": "back-end",
-                "correct": true
-            },
-            {
-                "answer": "front-end",
-                "correct": false
-            },
-            {
-                "answer": "operational system",
-                "correct": false
-            },
-            {
-                "answer": "database",
-                "correct": false
-            },
-        ]
-    },
-    {
-        "question": "A way to declare a variable in JavaScript:",
-        "answers": [
-          {
-            "answer": "$var",
-            "correct": false
-          },
-          {
-            "answer": "var",
-            "correct": true
-          },
-          {
-            "answer": "@var",
-            "correct": false
-          },
-          {
-            "answer": "#let",
-            "correct": false
-          },
-        ]
-    },
-    {
-        "question": "What is the id selector in CSS?",
-        "answers": [
-          {
-            "answer": "#",
-            "correct": true
-          },
-          {
-            "answer": ".",
-            "correct": false
-          },
-          {
-            "answer": "@",
-            "correct": false
-          },
-          {
-            "answer": "/",
-            "correct": false
-          },
-        ]
-    },
+  {
+    "question": "PHP was developed for what purpose?",
+    "answers": [
+      {
+          "answer": "back-end",
+          "correct": true
+      },
+      {
+          "answer": "front-end",
+          "correct": false
+      },
+      {
+          "answer": "operational system",
+          "correct": false
+      },
+      {
+          "answer": "database",
+          "correct": false
+      },
+    ]
+  },
+  {
+    "question": "A way to declare a variable in JavaScript:",
+    "answers": [
+      {
+        "answer": "$var",
+        "correct": false
+      },
+      {
+        "answer": "var",
+        "correct": true
+      },
+      {
+        "answer": "@var",
+        "correct": false
+      },
+      {
+        "answer": "#let",
+        "correct": false
+      },
+    ]
+  },
+  {
+    "question": "What is the id selector in CSS?",
+    "answers": [
+      {
+        "answer": "#",
+        "correct": true
+      },
+      {
+        "answer": ".",
+        "correct": false
+      },
+      {
+        "answer": "@",
+        "correct": false
+      },
+      {
+        "answer": "/",
+        "correct": false
+      },
+    ]
+  },
 ]
 
+// Quizz replacement for the first question
+function init() {
+  createQuestion(0);
+}
+
+// Create a question
+function createQuestion(i) {
+  // Clear the previous question
+  const oldButton = answersBox.querySelectorAll('button');
+  oldButton.forEach(function(btn) {
+    btn.remove();
+  })
+
+  // Change question text
+  const questionText = question.querySelector('#question-text');
+  const questionNumber = question.querySelector('#question-number');
+
+  questionText.textContent = questions[i].question;
+  questionNumber.textContent = i + 1;
+
+  
+}
+
+// Quizz startup
+init();
