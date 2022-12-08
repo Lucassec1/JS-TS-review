1480318afffd831875c442cfae28af91e2f0b964
Lucassec1 <lucasemmanuelcs@gmail.com>
2022-12-08 09:15:48 -0300

    :sparkles: feat: finishing the login from calculator

--- javascript/16-module/index.js
+++ javascript/16-module/index.js
@@ -11,7 +11,115 @@ class Calculator {
 
     // add digit to calculator screen
     addDigit(digit) {
-        console.log(digit)
+        // Check if current operation already has a dot
+        if (digit === '.' && this.currentOperationText.innerText.includes('.')) {
+            return;
+        }
+
+        this.currentOperation = digit;
+        this.updateScreen();
+    }
+
+    // Process all calculator operations
+    processOperation(operation) {
+        // check if current is empty
+        if (this.currentOperationText.innerText === "" && operation !== "C") {
+            // Change operation
+            if (this.previousOperationText.innerText !== '') {
+                this.changeOperation(operation);
+            }
+            return;
+        }
+
+        // Get current and previous value
+        let operationValue;
+        const previous = +this.previousOperationText.innerText.split(' ')[0];
+        const current = +this.currentOperationText.innerText;
+
+        switch(operation) {
+            case "+":
+                operationValue = previous + current
+                this.updateScreen(operationValue, operation, previous, current);
+                break;
+            case "-":
+                operationValue = previous - current
+                this.updateScreen(operationValue, operation, previous, current);
+                break;
+            case "*":
+                operationValue = previous * current
+                this.updateScreen(operationValue, operation, previous, current);
+                break;
+            case "/":
+                operationValue = previous / current
+                this.updateScreen(operationValue, operation, previous, current);
+                break;
+            case "DEL":
+                this.processDelOperator();
+                break;
+            case "CE":
+                this.processClearCurrentOperation();
+                break;
+            case "C":
+                this.processClearOperation();
+                break;
+            case "=":
+                this.processEqualOperation();
+                break;
+            default:
+                return;
+        }
+    }
+
+    // Change values of the calculator screen
+    updateScreen(
+        operationValue = null, 
+        operation = null,
+        current = null,
+        previous = null
+    ) {
+        if (operationValue === null) {
+            this.currentOperationText.innerText += this.currentOperation;
+        } else {
+            // check if value is zero, if it is just add current value
+            if (previous === 0) {
+                operationValue = current
+            }
+
+            // add current value to previous
+            this.previousOperationText.innerText = `${operationValue} ${operation}`;
+            this.currentOperationText.innerText = "";
+        }
+    }
+    // Change math operation
+    changeOperation(operation) {
+        const mathOperation = ['*', '/', '+', '-'];
+        if (!mathOperation.includes(operation)) {
+            return
+        }
+
+        this.previousOperationText.innerText = this.previousOperationText.innerText.slice(0, -1) + operation;
+    }
+
+    // Delete the last digit
+    processDelOperator() {
+        this.currentOperationText.innerText = this.currentOperationText.innerText.slice(0, -1);
+    }
+
+    // Clear current operation
+    processClearCurrentOperation() {
+        this.currentOperationText.innerText = "";
+    }
+
+    // Clear all operation
+    processClearOperation() {
+        this.currentOperationText.innerText = "";
+        this.previousOperationText.innerText = "";
+    }
+
+    // Process an operation
+    processEqualOperation() {
+        const operation = previousOperationText.innerText.split(' ')[1];
+        this.processOperation(operation);
     }
 }
 
@@ -24,7 +132,7 @@ buttons.forEach((btn) => {
         if(+value >= 0 || value === '.') {
             calc.addDigit(value);
         } else {
-            calc.addDigit(value);
+            calc.processOperation(value);
         }
     });
 });
