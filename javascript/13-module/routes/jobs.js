bb5b404bba6cc151dc25d260b68d9ecb4f871654
Lucassec1 <lucasemmanuelcs@gmail.com>
2022-12-15 09:16:09 -0300

    :sparkles: feat: post form

--- javascript/13-module/routes/jobs.js
+++ javascript/13-module/routes/jobs.js
@@ -1,14 +1,28 @@
 const express = require('express');
-const router = express.Router();
-const Job = require('../models/Job');
+const router  = express.Router();
+const Job     = require('../models/Job');
 
+// rota de teste
 router.get('/test', (req, res) => {
-    res.send('it worked out');
+  res.send('deu certo');
 });
 
+// detalhe da vaga -> view/1, view/2
+router.get('/view/:id', (req, res) => Job.findOne({
+  where: {id: req.params.id}
+}).then(job => {
+
+  res.render('view', {
+    job
+  });
+
+}).catch(err => console.log(err)));
+
+
+// form da rota de envio
 router.get('/add', (req, res) => {
-    res.render('add');
-});
+  res.render('add');
+})
 
 // Add job via post
 router.post('/add', async (req, res) => {
